3e4e6c05ffec126051d78509dff74efd08c0640c
xiongwilee <birthday10241120@qq.com>
2016-02-07 02:03:05 +0800

    update:添加regular方法
    update:添加regular方法

--- example/blog/controller/post.js
+++ example/blog/controller/post.js
@@ -4,6 +4,11 @@ module.exports = function* () {
   yield this.render('index', {
     title: 'Hello Koa-hornbill!'
   });
+
+  this.body = this.params;
 }
 // 配置index路由为get请求
 module.exports.__method__ = 'get';
+
+// 配置index路由的regular
+module.exports.__regular__ = '/:id/:test';
