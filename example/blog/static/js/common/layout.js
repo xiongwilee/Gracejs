e7018b08096d916097e606246ffb11cdc9356b20
xiongwilee <birthday10241120@qq.com>
2016-02-12 04:58:15 +0800

    update:更新koa-grace案例，支持swig，完善blog
    update:更新koa-grace案例，支持swig，完善blog

---
+++ example/blog/static/js/common/layout.js
@@ -0,0 +1,8 @@
+(function ($) {
+	var $headerBgB = $('#header_bg_b');
+/*	$(window).scroll(function () {
+	    var oVal = $(window).scrollTop() / 240;
+
+	    return $headerBgB.css('opacity', oVal);
+	});*/
+})(window.jQuery)
