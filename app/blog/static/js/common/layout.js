f3f145458c73d2e125ed9537cbfba20172451590
xiongwilee <birthday10241120@qq.com>
2016-02-16 14:35:17 +0800

    update:更新随机主题
    update:更新随机主题

--- app/blog/static/js/common/layout.js
+++ app/blog/static/js/common/layout.js
@@ -1,17 +1,36 @@
-(function ($) {
-	var init = function(){
-		bindEvent();
-	}
-	var bindEvent = function(){	
-		$('#search_form_wd').on('keyup', function(evt){
-			var thisVal = $(this).val();
-			$('#search_form_wd_hidden').val(thisVal + ' site:mlsfe.biz');
-		});
+(function($) {
+  var $header = $('#header');
 
-		$('#content_post').on('click','.post-link', function(evt){
-			console.log(this);
-		})
-	}
+  var init = function() {
+    setDefault();
+    bindEvent();
+  }
+  var bindEvent = function() {
+    $('#search_form_wd').on('keyup', function(evt) {
+      var thisVal = $(this).val();
+      $('#search_form_wd_hidden').val(thisVal + ' site:mlsfe.biz');
+    });
 
-	init();
+    $('#content_post').on('click', '.post-link', function(evt) {
+      console.log(this);
+    })
+  }
+  var setDefault = function() {
+    _setTheme();
+  }
+  var _setTheme = function() {
+    var theme = ['/static/image/bg_3_s.jpg',
+      '/static/image/bg_4_s.jpg',
+      '/static/image/bg_5_s.jpg',
+      '/static/image/bg_6_s.jpg',
+      '/static/image/bg_7_s.jpg',
+      '/static/image/bg_8_s.jpg'
+    ].sort(function() {
+      return Math.random() - 0.5;
+    }).slice(0, 1);
+
+    $header.css('background-image','url('+theme+')');
+  }
+
+  init();
 })(window.jQuery)
