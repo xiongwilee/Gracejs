d9ab037d81cd4287b2c3279967aaf90384b26685
xiongwilee <xiongwilee@foxmail.com>
2016-03-18 18:56:33 +0800

    update:config NODE_ENV to ENV

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let env = process.env.NODE_ENV || 'development';
+let env = process.env.ENV || 'development';
 let port = process.env.PORT || 3000;
 let DEBUG = process.env.DEBUG = process.env.DEBUG || 'koa-grace*'
 
