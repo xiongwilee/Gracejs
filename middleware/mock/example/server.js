5dcc48034636d99482fea5f2a2a6717af7856245
xiongwilee <xiongwilee@foxmail.com>
2016-10-18 12:23:49 +0800

    删掉middleware目录中的.babelrc文件

--- middleware/mock/example/server.js
+++ middleware/mock/example/server.js
@@ -1,6 +1,12 @@
 'use strict';
 
-require("babel-register");
+require("babel-register")({
+  "presets": ["es2015-node5"],
+  "plugins": [
+    "transform-async-to-generator",
+    "syntax-async-functions"
+  ]
+});
 
 const app = require('./app');
 
