d98d571268b3dfbebd8ed576a4b0ee917fe3dd1e
xiongwilee <xiongwilee@foxmail.com>
2017-04-06 00:16:38 +0800

    upd: 更新错误提示&版本号

--- app/iblog/controller/defaultCtrl.js
+++ app/iblog/controller/defaultCtrl.js
@@ -3,6 +3,8 @@
 const base = require('./base.js');
 
 module.exports = async function() {
+  this.assert(!!base.config.token, 401, 'Personal Access Token Undefined!');
+
   // 获取用户github信息
   await this.proxy({
     ownerInfo: `github_api:user?access_token=${base.config.token}`,
